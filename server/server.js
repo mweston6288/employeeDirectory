58cea348c4a6a02b3b253a1b84b84d895b6470aa
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-04-29 19:45:07 -0400

    More edits that I hope will get heroku to work

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 const cors = require("cors")
-const PORT = 3000;
+const PORT = process.env.PORT||3000;
 
 const app = express();
 
@@ -21,7 +21,9 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/employee", {
 });
 
 app.use(require("./routes/apiRoutes.js"))
-
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static('../build'))
+}
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
 });
