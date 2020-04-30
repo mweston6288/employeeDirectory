d1aed33c64de793b902483721855fa322ef2b4de
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-04-29 20:50:45 -0400

    Changed axios

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,15 +1,13 @@
 import axios from "axios"
 
-const api = axios.create({
-    baseURL: process.env.MONGODB_URI || 'http://localhost:8080',
-})
+
 // Call on the api routes in server/routes/apiRoutes
-export const addEmployee = ({ name, DOB, role, email }) => api.post("./api/employee", {name,DOB,role,email});
-export const getAllEmployees = ()=>api.get("./api/employee");
+export const addEmployee = ({ name, DOB, role, email }) => axios.post("./api/employee", {name,DOB,role,email});
+export const getAllEmployees = ()=>axios.get("./api/employee");
 // id is the data field to sort by
-export const getEmployeesBySort = (id) => api.get("./api/employee/"+id)
+export const getEmployeesBySort = (id) => axios.get("./api/employee/"+id)
 // id is the name to filter the search by
-export const getEmployeesByName = (id) => api.get("./api/employee/name/" + id)
+export const getEmployeesByName = (id) => axios.get("./api/employee/name/" + id)
 
 
 const apis = {
