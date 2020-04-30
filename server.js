733a03a5ba1e4279d98fd4a74bc8ff0dbb76eeda
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-04-29 20:40:38 -0400

    Changed default ports

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 const cors = require("cors")
-const PORT = process.env.MONGODB_URI || 3000;
+const PORT = process.env.MONGODB_URI || 8080;
 
 const app = express();
 
